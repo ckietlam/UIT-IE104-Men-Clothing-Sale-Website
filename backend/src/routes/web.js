ef834d6f04b08bd4c10d57aed0cadf4c74fd023c
noahihi203 <da02470247@gmail.com>
2024-11-17 14:27:51 +0700

    added route /admin

--- backend/src/routes/web.js
+++ backend/src/routes/web.js
@@ -1,8 +1,11 @@
 import express from "express";
+import userController from "../controllers/userController"
 
 let router = express.Router();
 
 let initWebRouters = (app) => {
+  router.get("/admin", userController.getAdmin);
+
   return app.use("/", router);
 };
 
