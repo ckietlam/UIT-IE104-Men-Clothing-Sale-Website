b0a885ab71671c7b174257ce2419dcff07404321
chvyl <22520755@gm.uit.edu.vn>
2024-11-27 15:05:04 +0700

    Feature/Update lại link tới hình ảnh, update hình ảnh trang view-all, bổ sung trang product-view-varsity

---
+++ frontend/public/scripts/product-view-blazer.js
@@ -0,0 +1,6 @@
+function changeImage(imageName) {
+    // Tìm thẻ hình ảnh chính (main-image)
+    const mainImage = document.getElementById("main-image");
+    // Thay đổi thuộc tính src để hiển thị ảnh mới
+    mainImage.src = `../../public/images/temp/${imageName}`;
+}
