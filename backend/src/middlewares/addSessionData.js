7034b238bb7ce356c3886bcd60fb843fde924eb9
Chung Kiet Lam <153978151+ckietlam@users.noreply.github.com>
2024-12-03 16:55:24 +0700

    Revert "Feature/login back&front (#6)"
    This reverts commit fe46e0cc918f0bcbb9a8a44e2d3ee9c5b490280f.

--- backend/src/middlewares/addSessionData.js
+++
@@ -1,8 +0,0 @@
-const addSessionData = (req, res, next) => {
-    res.locals.isAuthenticated = req.session.authenticated || false;
-    res.locals.user = req.session.user || null;
-    next();
-};
-
-export default addSessionData;
-  
