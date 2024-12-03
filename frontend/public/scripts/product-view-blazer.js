e6b49c1c1355449eca4e679c89a4c18eaf159cea
chvyl <22520755@gm.uit.edu.vn>
2024-12-03 21:49:23 +0700

    Feature/Update trang mô tả sản phẩm bằng ejs

--- frontend/public/scripts/product-view-blazer.js
+++ frontend/public/scripts/product-view-blazer.js
@@ -2,5 +2,5 @@ function changeImage(imageName) {
     // Tìm thẻ hình ảnh chính (main-image)
     const mainImage = document.getElementById("main-image");
     // Thay đổi thuộc tính src để hiển thị ảnh mới
-    mainImage.src = `../../public/images/temp/${imageName}`;
+    mainImage.src = `../../public/images/temp/assets/products/shirts/aokhoac/ao_blazer/${imageName}`;
 }
